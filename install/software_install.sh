f9b9d40b2e2dc08b0e56eb81254381d6a3fab61a
Ming Tsai <mingster.tsai@gmail.com>
2019-09-28 12:48:43 +0800

    add zsh

--- install/software_install.sh
+++ install/software_install.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
 #intall general shit (must have list)
-sudo apt-get install -y curl zsh vim tree nmap tmux git figlet fonts-inconsolata rsync
+sudo apt-get install -y curl zsh vim tree nmap tmux git figlet fonts-inconsolata rsync zsh
+
+# Make ZSH to be the  default Shell
+#chsh -s /bin/zsh
+
+# Installing Oh My ZSH via wget
+#sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# powerlevel9k theme
+#git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 #bash
 sudo apt-get install bash bash-completion
