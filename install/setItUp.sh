4894fb6dcfb030812dcfb8b83cfe784056f30a1b
Ming Tsai <mingster.tsai@gmail.com>
2020-05-23 16:58:54 +0800

    add back git pull

--- install/setItUp.sh
+++ install/setItUp.sh
@@ -2,9 +2,7 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
-#git pull origin master;
-
-
+git pull origin master;
 
 function doIt () {
 rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms .. ~;
